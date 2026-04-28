9cac331164cee3d8be543f25845d58bf797418f9
Saksham-ux0807 <sakshamtripathi611@gmail.com>
2026-04-29 02:06:08 +0530

    Update setupTests.ts

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,1 @@
-import '@testing-library/jest-dom'
 
-// jsdom does not implement scrollIntoView; provide a no-op stub so components
-// that call it (e.g. AutocompleteDropdown's keyboard-focus scroll) don't throw.
-window.HTMLElement.prototype.scrollIntoView = function () {};
